972693c1795d0a4a6ed3a719d58e0870082ad6c8
Turner Hayes <dev@turnerhayes.com>
2020-09-13 14:38:57 -0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KeyboardComponent } from './keyboard/keyboard.component';
+import { WhiteKeyPipe } from './keyboard/white-key.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KeyboardComponent,
+    WhiteKeyPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: Window,
+      useValue: window,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
